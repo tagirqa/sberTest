ae143a0b3ae2e12a6d1a6148b1ff4ad562697ca8
tagirqa <smatrix2005@yandex.ru>
2020-06-07 14:05:37 +0300

    init

--- src/main/java/sber/ru/steps/BaseSteps.java
+++ src/main/java/sber/ru/steps/BaseSteps.java
@@ -2,8 +2,12 @@ package sber.ru.steps;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import sber.ru.TestProperties;
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class BaseSteps {
@@ -13,10 +17,29 @@ public class BaseSteps {
     }
     public static WebDriverWait wait;
 
-    public static void setUp(){
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
 
-        driver = new ChromeDriver();
+
+    public static void setUp(){
+        Properties properties = TestProperties.getInstance().getProperties();
+
+        String browser = properties.getProperty("browser", "opera");
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+                driver = new ChromeDriver();
+                break;
+            case "opera":
+                System.setProperty("webdriver.opera.driver", "drivers/operadriver");
+                driver = new OperaDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+                driver = new FirefoxDriver();
+                break;
+        }
+
+
+
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
